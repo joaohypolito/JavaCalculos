908cd4b01903bd11d4a8ad594558caec5de43fb2
João Pedro A. Hypólito <113153174+joaohypolito@users.noreply.github.com>
2024-02-27 12:39:43 -0300

    Delete directory

--- tcc/src/main/java/com/tcc/api/repository/PrestadorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tcc.api.repository;
-
-import com.tcc.api.model.PrestadorModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrestadorRepository extends JpaRepository<PrestadorModel, Long> {
-}
